6204448d4dace557900fa991a3ee1eec140486b8
Ajiniyaz Djiemuratov <lmt9229@yahoo.com>
2024-04-05 14:47:32 +0500

    added python file

---
+++ test.py
@@ -0,0 +1,17 @@
+name = "Ajiniyaz"
+result = 2 + 4
+print(name, result)
+print("a little name thing".capitalize())
+
+
+def add_one(num):
+    if num >= 9:
+        return num + 1
+
+    total = num + 1
+    print(total)
+    return add_one(total)
+
+
+mynewtotal = add_one(0)
+print(mynewtotal)
